ffcaedf0d773241f5888cc4507cdf7d237c0eee2
vittoriodurigutti <vittodutti@gmail.com>
2025-11-04 11:10:16 -0300

    actualizacion de repositorio, y reubicacon de front y back mal generados

--- backend/src/middlewares/data.middlewares.js
+++
@@ -1,2 +0,0 @@
-// Este archivo es para la gestion/parcseo de datos.
-// Puede que nos haga falta cuando llamemos datos para los graficos, pero de momento lo dejamos vacio.
--- backend/src/sw/uWebSockets.js
+++
@@ -1,2 +0,0 @@
-// Este archivo tiene que verc con el manejo de los websockets. 
-// Hasta que no entremos en cuestiones de graficos, lo mantenemos vacio
